b63447e16d15063191aa7aefbffb262130d4bbf0
Akeem Hart <alhart@fnal.gov>
2024-02-29 13:44:33 -0600

    Removing unnecessary components from custom study

--- studies/PerEventVarByGENIELabel2D.h
+++ studies/PerEventVarByGENIELabel2D.h
@@ -15,15 +15,6 @@ class PerEventVarByGENIELabel2D: public Study
     //PerMichelVarByGENIELabel fills a histogram with 1 entry per Michel with some variable calculated from that Michel.  Your function will get to see the CVUniverse, the MichelEvent (= reconstructed Michels), and which Michel it's looping over.
     using reco_t = std::function<double(const CVUniverse&, const MichelEvent&)>;
 
-    /*PerEventVarByGENIELabel2D(reco_t reco_x, reco_t reco_y, const std::string& varName, const std::string& varUnits, const int nBins, const double minBin, const double maxBin, const std::map<std::string, std::vector<CVUniverse*>>& univs): Study(), fReco_x(reco_x), fReco_y(reco_y)
-    {
-      std::map<int, std::string> GENIELabels = {{1, "QE"},
-                                                {8, "2p2h"},
-                                                {2, "RES"},
-                                                {3, "DIS"}};
-      m_VarToGENIELabel = new util::Categorized<HIST, int>(varName, varName + " [" + varUnits + "]", GENIELabels, nBins, minBin, maxBin, univs);
-    }*/
-
     PerEventVarByGENIELabel2D(reco_t reco_x, reco_t reco_y, const std::string& varName, const std::string& varUnits, const std::vector<double> xBins, const std::vector<double> yBins, const std::map<std::string, std::vector<CVUniverse*>>& univs): Study(), fReco_x(reco_x), fReco_y(reco_y)
     {
       std::map<int, std::string> GENIELabels = {{1, "QE"},
@@ -54,9 +45,7 @@ class PerEventVarByGENIELabel2D: public Study
 
     //Overriding base class functions
     //Do nothing for now...  Good place for data comparisons in the future. 
-    void fillSelected(const CVUniverse& univ, const MichelEvent& evt, const double weight) {
-        (*m_VarToGENIELabel)[univ.GetInteractionType()].FillUniverse(&univ, fReco_x(univ, evt), fReco_y(univ, evt), 1);
-    }
+    void fillSelected(const CVUniverse& univ, const MichelEvent& evt, const double weight) {}
 
     //All of your plots happen here so far.
     void fillSelectedSignal(const CVUniverse& univ, const MichelEvent& evt, const double weight)
